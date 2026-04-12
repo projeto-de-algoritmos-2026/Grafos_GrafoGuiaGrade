3fbc338b32f2783bcf87f0fe360e92d47f744943
guilhermezan42 <guilhermeczanella@gmail.com>
2026-04-12 03:11:05 -0300

    adiciona função dfs

--- src/main.py
+++ src/main.py
@@ -10,5 +10,23 @@ def cria_grafo_pre_requisitos(disciplinas, pre_requisitos):
     for pre_requisito, materia in pre_requisitos:
         grafo[pre_requisito].append(materia)
     return grafo
-grafo = cria_grafo_pre_requisitos(disciplinas,pre_requisitos)
-print(grafo)
+grafo_pre_requisitos = cria_grafo_pre_requisitos(disciplinas,pre_requisitos)
+print(grafo_pre_requisitos)
+
+def dfs(grafo):
+    visitados = []
+    arvore = []
+    for materia in grafo:
+        if materia not in visitados:
+            dfs_visit(grafo,materia, visitados, arvore)
+    return visitados,arvore
+def dfs_visit(grafo, materia, visitados, arvore):
+    visitados.append(materia)
+    for vizinho in grafo[materia]:
+        if vizinho not in visitados:
+            arvore.append((materia,vizinho))
+            dfs_visit(grafo,vizinho, visitados, arvore, )
+
+ordem_visitados, arvore_dfs = dfs(grafo_pre_requisitos)
+print("ordem de visitação dfs", ordem_visitados)
+print("arvore dfs", arvore_dfs)
