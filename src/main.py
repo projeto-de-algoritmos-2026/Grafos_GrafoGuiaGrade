b4befc1ba374cbe121c5621e4de59827f7ec0872
guilhermezan42 <guilhermeczanella@gmail.com>
2026-04-12 01:40:49 -0300

    adiciona função cria grafo de disciplinas e pre-requisitos

--- src/main.py
+++ src/main.py
@@ -1,3 +1,14 @@
 from data import disciplinas,pre_requisitos
-print(disciplinas)
-print(pre_requisitos)
+
+
+def cria_grafo(disciplinas, pre_requisitos):
+    #& inicializa grafo
+    grafo = {}
+    for disciplina in disciplinas:
+        grafo[disciplina] = []
+    #& adiciona vizinhos a cada nó
+    for pre_requisito, materia in pre_requisitos:
+        grafo[pre_requisito].append(materia)
+    return grafo
+grafo = cria_grafo(disciplinas,pre_requisitos)
+print(grafo)
